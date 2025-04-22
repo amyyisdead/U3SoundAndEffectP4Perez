6bb2750a6a44e67f351dd0d8e7078c274e100ece
ELMilkMan <754989@student.dusd.net>
2025-04-22 11:43:26 -0700

    Start of unit 3

---
+++ P4U3SoundAndEffectPerez/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obsticalPrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstical", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnObstical()
+    {
+        Instantiate(obsticalPrefab, spawnPos, obsticalPrefab.transform.rotation);
+    }
+}
